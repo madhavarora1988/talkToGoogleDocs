17bf139a766e2103d0a8fe4a583cb932a46277e5
madhavarora <madhavarora1988@gmail.com>
2024-06-09 21:12:54 -0400

    feat: changes for updating the implementation

--- main.py
+++ main.py
@@ -5,28 +5,31 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-from langchain_community.document_loaders import YoutubeLoader
-import pandas as pd
 from langchain_anthropic import ChatAnthropic
-from metrics.evaluate_flesch_kincaid_score import flesch_kincaid_grade_level
+from langchain_googledrive.document_loaders import GoogleDriveLoader
 
-# openai api key
-os.environ["OPENAI_API_KEY"] = "<OpenAI Key>"
+os.environ["OPENAI_API_KEY"] = "<OpenAI key>"
 os.environ[
     "ANTHROPIC_API_KEY"] = "<AnthropicAI Key>"
 
 
-def load_youtube_video(video_url):
-    loader = YoutubeLoader.from_youtube_url(
-        video_url, add_video_info=True
+def load_google_drive_data(folder_id):
+    loader = GoogleDriveLoader(
+        # provide the path to the credentials file, I have it in the project root.
+        gdrive_api_file="credentials.json",
+        folder_id=folder_id,
+        # Optional: configure whether to recursively fetch files from subfolders. Defaults to False.
+        recursive=False
     )
-    data = loader.load()
-    return data
+    loaded_data = loader.load()
+    print("loaded data ", loaded_data)
+    return loaded_data
 
 
 def persist_data_in_chroma(persist_directory, data):
     # various supported model options are available
     embedding_function = OpenAIEmbeddings()
+    print('data: ', data)
 
     # Chroma instance takes in a directory location, embedding function and ids-> list of documentIds
     chroma_db = Chroma.from_documents(data,
@@ -39,7 +42,8 @@ def create_llm(model_name, **kwargs):
     # Map model names to their corresponding class constructors
     model_map = {
         "claude-3-sonnet-20240229": ChatAnthropic,
-        "gpt-3.5-turbo": ChatOpenAI
+        "gpt-3.5-turbo": ChatOpenAI,
+        "gpt-4o": ChatOpenAI,
         # Add more models here
     }
     # Get the constructor for the given model_name or raise a ValueError if not found
@@ -50,7 +54,7 @@ def create_llm(model_name, **kwargs):
         raise ValueError("Unsupported model_name")
 
 
-def summarize_data(db, llm):
+def query_data(db, llm, query):
     print("summarizing data with llm : ", llm)
 
     chatbot_chain = RetrievalQA.from_chain_type(
@@ -60,7 +64,7 @@ def summarize_data(db, llm):
     )
 
     template = """
-    respond as clearly as possible {query}?
+    respond as clearly as possible and in max 200 words {query}?
     """
 
     prompt = PromptTemplate(
@@ -69,72 +73,34 @@ def summarize_data(db, llm):
     )
 
     summary = chatbot_chain.run(
-        prompt.format(query="summarize the video transcript in 100 words?")
+        prompt.format(query=query)
     )
     return summary
 
 
-def read_excel(file_path):
-    # Load the Excel file
-    df = pd.read_excel(file_path)
-    youtube_links = df['Youtube Links'].tolist()
-    return youtube_links
 
 
-def update_excel_row(file_path, row_index, title, description, model_name):
-    # Load the Excel file
-    df = pd.read_excel(file_path)
 
-    # Check if row_index is valid
-    if row_index >= len(df):
-        raise IndexError("The row_index is out of bounds of the Excel file")
 
-    # Update the 'Title' and 'Description' columns for the specified row
-    df.at[row_index, 'Model'] = model_name
-    df.at[row_index, 'Title'] = title
-    df.at[row_index, 'Description'] = description
-
-    # Write the updated DataFrame back to the Excel file
-    df.to_excel(file_path, index=False)
 
 def process_args():
-    parser = argparse.ArgumentParser(description="Process model_name and file_path values")
-    parser.add_argument('model_name', type=str, help='The name of the model to use')
-    parser.add_argument('--file_path', type=str, default="YoutubeSummaries.xlsx",
-                        help='The path to the Excel file with YouTube summaries')
+    parser = argparse.ArgumentParser(description="Process model_name")
+    parser.add_argument('--model_name', type=str, help='The name of the model to use')
     return parser
-
-def extract_title(data):
-    try:
-        title = data[0].metadata.get('title', "") if data else ""
-    except (IndexError, AttributeError, KeyError):
-        title = ""
-    return title
 
 
 if __name__ == '__main__':
     args = process_args().parse_args()
-    file_path = args.file_path
     model_name = args.model_name
+    folder_id = '<Google Drive Folder Id>'
 
-    links = read_excel(file_path)
-    response_list = []
-    print(links)
-    for index, link in enumerate(links):
-        data = load_youtube_video(link)
-        title = extract_title(data)
-        db = persist_data_in_chroma('db', data)
+    # Load the data from Google Drive
+    data = load_google_drive_data(folder_id)
+    # Persist the data in Chroma
+    db = persist_data_in_chroma('db', data)
+    # configure the respective model
+    llm = create_llm(model_name, temperature=0, max_tokens=500)
+    # TODO: Persist the db, and use that instance to query
+    response = query_data(db, llm, "tell me about the TimeBench, and provide the path to the google drive document?")
 
-        llm = create_llm(model_name, temperature=0, max_tokens=500)
-        summary = summarize_data(db, llm)
-        flesch_kincaid_score = flesch_kincaid_grade_level(summary)
-
-        json_resp = {"index": index,
-                     "title": title,
-                     "summary": summary,
-                     "flesch_kincaid_score": flesch_kincaid_score
-                     }
-        response_list.append(json_resp)
-
-        update_excel_row(file_path, index, title, summary, model_name)
-    print('response from model: ', response_list)
+    print('response from model: ', response)
